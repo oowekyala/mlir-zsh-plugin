51568cb4ec64b5ff6b3b3b22ef1aa743778bfcb4
Clément Fournier <clement.fournier@tu-dresden.de>
2025-11-18 14:16:15 +0100

    Add aliases with completion that call pygments

--- mlir.plugin.zsh
+++ mlir.plugin.zsh
@@ -2,9 +2,21 @@
 
 autoload -Uz pygmentize_mlir
 
-# Compdef for MLIR
-compdef _mlir_opt tilefirst-opt mlir-opt cinm-opt
+# Array of names that are to be interpreted as mlir-opt programs
+mlir_opt_programs=(mlir-opt tilefirst-opt cinm-opt)
 
+# Wrap the underlying program to colorize the output
+_mlir_opt_with_colors() {
+  command "$@" | pygmentize_mlir
+}
+
+for prog in ${mlir_opt_programs[@]}; do
+  alias $prog="_mlir_opt_with_colors $prog"
+done
+
+# Compdef for MLIR
+compdef _mlir_opt ${mlir_opt_programs[@]} 
+compdef _mlir_opt _mlir_opt_with_colors
 
 # TODO 
 #  - wrapper for auto colorization, allow disabling it with zstyle
