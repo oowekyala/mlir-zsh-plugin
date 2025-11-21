3d45a488186fb5e3c54cbcae20486d775dc2df59
Clément Fournier <clement.fournier@tu-dresden.de>
2025-11-21 12:07:04 +0100

    Move wrap_mlir_opt, elide constants automatically

--- mlir.plugin.zsh
+++ mlir.plugin.zsh
@@ -10,25 +10,15 @@ local mlir_opt_programs
 zstyle -a ':plugins:mlir' mlir_opt_programs mlir_opt_programs
 ((${mlir_opt_programs:=mlir-opt})) # default value
 
-autoload -Uz pygmentize_mlir
-autoload -Uz __find_mlir_opt_cmd
-
-# A function to wrap an MLIR frontend command with colorized output.
-function wrap_mlir_opt_with_colors() {
-  if ! zstyle -T ':plugins:mlir:pygments' enabled || (($@[(I)--help*])); then
-    command "$@"
-  else
-    command "$@" | pygmentize_mlir
-  fi
-}
+autoload -Uz wrap_mlir_opt
 
 for prog in ${mlir_opt_programs[@]}; do
   if zstyle -T ':plugins:mlir:pygments' enabled; then
-    alias $prog="wrap_mlir_opt_with_colors $prog"
+    alias $prog="wrap_mlir_opt $prog"
   fi
   zstyle ":completion::complete:$prog:"'values*' menu yes
 done
 
 # Compdef for MLIR
 compdef _mlir_opt ${mlir_opt_programs[@]} 
-compdef _precommand wrap_mlir_opt_with_colors
+compdef _precommand wrap_mlir_opt
